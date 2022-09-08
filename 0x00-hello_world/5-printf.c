5439c3ce368a5935fd25b21518a9747f286b92bc
toseth97 <oluwasheun9721@gmail.com>
2022-09-08 09:11:01 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**main - Enrty point
+ *
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the out come is a piece of art,");
+	return (0);
+}
+
