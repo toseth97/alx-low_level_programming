e7da7a52fede2ae60daf1b6c4ea9bbf08a9c3801
toseth97 <oluwasheun9721@gmail.com>
2022-09-08 21:36:13 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,6 @@
 
 int main(void)
 {
-	write(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fwrite(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
