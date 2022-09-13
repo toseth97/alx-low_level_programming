15e5f1dae46fec5cc0f082ca59e57cac6f9b4ece
toseth97 <oluwasheun9721@gmail.com>
2022-09-13 06:45:04 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+/*
+ * main - Entry point 
+ *
+ */
+#include "main.h"
+
+/*
+ * Description -  Print a - z 10x
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char letter = 'a';
+
+	for (i = 0; i < 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
