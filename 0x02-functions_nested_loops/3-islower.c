af6567b9acb99060af46d97dbea7980dc911ad1f
Lawal Tobiloba <65098660+toseth97@users.noreply.github.com>
2022-09-13 15:33:00 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,11 @@
  * main - Enty point
  */
 #include "main.h"
-/*
- * _islower: It check if it is lower
+/**
+ * _islower - Checks if a character is lowercase.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is lowercase, 0 otherwise.
  */
 int _islower(int c)
 {
