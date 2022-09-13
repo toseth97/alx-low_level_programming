c34ee9bcf050757a0d3df69c9f99721586923d79
toseth97 <oluwasheun9721@gmail.com>
2022-09-13 07:28:33 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+/**
+ * main - Enty point
+ */
+#include "main.h"
+/*
+ * _islower: It check if it is lower
+ */
+int _islower(int c)
+{
+	if ( c >= 'a' && c <= 'z')
+	{
+		return 1;
+	}
+	else
+	{
+		return (0);
+	}
+}
