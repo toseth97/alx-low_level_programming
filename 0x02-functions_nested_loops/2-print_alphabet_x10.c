4db5bbaa144a881babc655744fc0565010a8ba0b
Lawal Tobiloba <65098660+toseth97@users.noreply.github.com>
2022-09-13 15:07:17 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints the alphabet in lowercase, followed by a new line.
+ * print_alphabet_x10 - prints the alphabet
  */
 void print_alphabet_x10(void)
 {
