9f198e24eb96a005613a153de70e2e4f50f47367
toseth97 <oluwasheun9721@gmail.com>
2022-09-13 06:32:38 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,17 @@
+/**
+ * main- Entry
+ *
+ */
+
 #include "main.h"
 
 /**
  * main - Entry point
  *
- * Description: This is the description
+ * print_alphabet: Prints the character and add next line
  *
  * Return: Always 0 (Success)
  */
-
 void print_alphabet(void)
 {
 	char c = 'a';
