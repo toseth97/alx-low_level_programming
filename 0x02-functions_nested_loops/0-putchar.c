3a997938c2ad8a4daa4e2181c2a59d01cd401718
toseth97 <oluwasheun9721@gmail.com>
2022-09-13 03:51:30 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <main.h>
+
+/**
+ * main - It ususes the _pputchar to print out.
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet()
+{
+	for (int i = 'a'; i <= 'z'; i++)
+	{
+		_putchar("%s\n",&i);
+	}
+}
