bba1d3f85aa1cf53f2f7cd2851b89665ca2c55da
toseth97 <oluwasheun9721@gmail.com>
2022-09-15 04:47:18 -0700

    debugg

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,22 @@
+/**
+ * File: 0-main.c
+ * Auth: Lawal
+ */
+
+#include "main.h"
+
+/**
+ * main - test function that prints
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+i}
