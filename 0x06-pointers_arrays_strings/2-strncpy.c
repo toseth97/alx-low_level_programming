e7c969f1c22063b8e249c7918678d9261433bf9b
toseth97 <oluwasheun9721@gmail.com>
2022-09-22 05:54:05 -0700

    22222

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,15 +14,13 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; src[i] != '\0'; i++)
 	{
-		if (i < n)
+		if (i < n && src[i] != '\0')
 		{
 			dest[i] = src[i];
 		}
-		while (i < n)
-		{
-			dest[i++] = '\0';
-		}
 	}
+
+	dest[i] = '\0';
 
 	return (dest);
 }
