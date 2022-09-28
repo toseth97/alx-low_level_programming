3855137ac762e8715f268acb71e121aa26d6de30
toseth97 <oluwasheun9721@gmail.com>
2022-09-28 09:10:41 -0700

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,8 +8,15 @@
 
 int factorial(int n)
 {
-	if (n < 0)
-		return (-1);
+	int fac = 1;
+
+	if (n > 0)
+	{
+		fact *= n;
+		factorial(n - 1);
+	}
 	else
-		return n * factorial(n - 1);
+		return (-1);
+
+	return (fact);
 }
