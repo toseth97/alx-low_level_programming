836db3853ab305127935f4d9336b900ffe6d3623
toseth97 <oluwasheun9721@gmail.com>
2022-09-28 19:46:12 -0700

    102-odd

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+int squareroot(int n, int i);
+
+/**
+ * _sqrt_recursion - entry
+ * squareroot - check perfect square root
+ * @n: integer
+ * @i: counter
+ * Return: ALways 0 (sueccess)
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return (squareroot(n, (n - 1) / 2));
+}
+
+int squareroot(int n, int i)
+{
+	if ( i < 1)
+		return (-1);
+	else if (i * i == n)
+		return (i);
+	else
+		return (squareroot(n, i - 1));
+}
