8585b0e6fdb02f0accadfe15fda8cd223a267a69
toseth97 <oluwasheun9721@gmail.com>
2022-10-01 06:19:51 -0700

    abeg just go

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -9,8 +9,8 @@ int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
 void _puts(char *s);
-char *_strcat(char *dest, char *src);
-int _atoi(void);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -21,5 +21,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
 #endif
