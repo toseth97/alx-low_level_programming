ac4579665ed9b3552c9c8f2450837a4639f87413
toseth97 <oluwasheun9721@gmail.com>
2022-10-04 03:41:02 -0700

    0-create_array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - Create a new array from pointer
+ * @size: specifies the size of the memory to be allocated using malloc
+ * @c: character to copy
+ * Return: returns char
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ar;
+	int i;
+
+	ar = malloc(size * sizeof(char));
+
+	if (ar != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			ar[i] = c;
+		}
+	}
+
+	return (ar);
+}
