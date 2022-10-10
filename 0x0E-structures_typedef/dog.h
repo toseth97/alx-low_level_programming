74acf6bec84423bbd3c7c032dc9f610e2fd85340
toseth97 <oluwasheun9721@gmail.com>
2022-10-10 08:02:22 -0700

    Structs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog{
+/**
+ * struct dog - structure
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
