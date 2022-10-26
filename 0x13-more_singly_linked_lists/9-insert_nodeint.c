4849d84aecd96d98ff105e267983aac0f67129c5
toseth97 <oluwasheun9721@gmail.com>
2022-10-25 19:17:53 -0700

    Insert node at specific index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,43 +1,38 @@
 #include "lists.h"
+#include <stdlib.h>
 /**
- * insert_nodeint_at_index - Entry Point
- * @head: head
- * @idx: index
- * @n: data
- * Return: 0
- */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+  * insert_nodeint_at_index - insert a node at index
+  * @head: head of the list
+  * @index: location to insert node
+  * @n: value of the inserted node
+  * Return: pointer to head of list
+  */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 {
-	unsigned int count = 0;
+	listint_t *current, *new;
 
-	listint_t *new_node;
-	listint_t *temp;
-
-	if (*head == NULL)
+	current = *head;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
 		return (NULL);
-
-	temp = *head;
-
-	while (temp != NULL)
+	new->n = n;
+	if (index == 0)
 	{
-		if (count == idx - 1)
-		{
-			new_node = malloc(sizeof(listint_t));
-			if (new_node == NULL)
-			{
-				return (NULL);
-			}
-			else
-			{
-				new_node->n = n;
-				new_node->next = temp->next;
-				temp->next = new_node;
-				return (new_node);
-			}
-		}
-		if (temp->next != NULL)
-			temp = temp->next;
-		count++;
+		new->next = current;
+		*head = new;
+		return (*head);
 	}
-	return (NULL);
+	while (index > 1)
+	{
+		current = current->next;
+		index--;
+		if (!current)
+		{
+			free(new);
+			return (NULL);
+		}
+	}
+	new->next = current->next;
+	current->next = new;
+	return (new);
 }
