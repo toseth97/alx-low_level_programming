f18de415216d33d8fe1122f32fc76979215b7328
toseth97 <oluwasheun9721@gmail.com>
2022-10-25 18:32:12 -0700

    Get node at index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Get node at a specific index;
+ *
+ * @head: Structure pointer
+ * @index: index to lookup
+ * Return: Node at the index
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	int i = 0;
+
+	listint_t *temp = malloc(sizeof(listint_t));
+
+	if (head == NULL)
+		return (NULL);
+
+	temp = head;
+
+	for (i = 0; i < index; i++)
+		temp = temp->next;
+
+	return (temp);
+}
